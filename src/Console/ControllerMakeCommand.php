669f11b94712448c189d7485a4fcf63c346a7b4f
atehnix <atehnix@gmail.com>
2018-02-05 00:21:06 +0300

    Update make:controller command

--- src/Console/ControllerMakeCommand.php
+++ src/Console/ControllerMakeCommand.php
@@ -7,26 +7,37 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace ATehnix\LaravelStubs\Console;
 
 use Illuminate\Routing\Console\ControllerMakeCommand as BaseControllerMakeCommand;
+use Illuminate\Support\Str;
+use InvalidArgumentException;
 
 class ControllerMakeCommand extends BaseControllerMakeCommand
 {
+    /**
+     * The Laravel application instance.
+     *
+     * @var \Illuminate\Foundation\Application
+     */
+    protected $laravel;
+
     /**
      * Get the stub file for the generator.
      *
      * @return string
      */
-    
     protected function getStub()
     {
-        if ($this->option('model')) {
-            $stub = config('stubs.path').'/controller.model.stub';
+        if ($this->option('parent')) {
+            $stub = config('stubs.path') . '/controller.nested.stub';
+        } elseif ($this->option('model')) {
+            $stub = config('stubs.path') . '/controller.model.stub';
         } elseif ($this->option('resource')) {
-            $stub = config('stubs.path').'/controller.stub';
+            $stub = config('stubs.path') . '/controller.stub';
         } else {
-            $stub = config('stubs.path').'/controller.plain.stub';
+            $stub = config('stubs.path') . '/controller.plain.stub';
         }
 
         return file_exists($stub) ? $stub : parent::getStub();
@@ -35,11 +46,33 @@ class ControllerMakeCommand extends BaseControllerMakeCommand
     /**
      * Get the default namespace for the class.
      *
-     * @param  string  $rootNamespace
+     * @param  string $rootNamespace
      * @return string
      */
     protected function getDefaultNamespace($rootNamespace)
     {
-        return $rootNamespace.config('stubs.namespaces.controller');
+        return $rootNamespace . config('stubs.namespaces.controller');
+    }
+
+    /**
+     * Get the fully-qualified model class name.
+     *
+     * @param  string $model
+     * @return string
+     */
+    protected function parseModel($model)
+    {
+        if (preg_match('([^A-Za-z0-9_/\\\\])', $model)) {
+            throw new InvalidArgumentException('Model name contains invalid characters.');
+        }
+
+        $model = trim(str_replace('/', '\\', $model), '\\');
+        $namespace = $this->laravel->getNamespace() . \trim(config('stubs.namespaces.model'), '\\') . '\\';
+
+        if (!Str::startsWith($model, $namespace)) {
+            $model = $namespace . $model;
+        }
+
+        return $model;
     }
 }
