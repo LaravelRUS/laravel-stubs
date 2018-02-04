0ada05f256ba0c950dbcef0b088f9b9f89bd1176
atehnix <atehnix@gmail.com>
2018-02-05 00:25:43 +0300

    Add make:rule command

---
+++ src/Console/RuleMakeCommand.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of laravel-stubs package.
+ *
+ * @author ATehnix <atehnix@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ATehnix\LaravelStubs\Console;
+
+use Illuminate\Foundation\Console\RuleMakeCommand as BaseRuleMakeCommand;
+
+class RuleMakeCommand extends BaseRuleMakeCommand
+{
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        $stub = config('stubs.path') . '/rule.stub';
+
+        return file_exists($stub) ? $stub : parent::getStub();
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . config('stubs.namespaces.rule');
+    }
+}
