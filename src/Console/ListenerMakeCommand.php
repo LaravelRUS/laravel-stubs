6afb26c521be3e138f52ad18f53c5f887664cbb9
atehnix <atehnix@gmail.com>
2018-02-05 00:22:00 +0300

    Update make:listener command

--- src/Console/ListenerMakeCommand.php
+++ src/Console/ListenerMakeCommand.php
@@ -7,6 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace ATehnix\LaravelStubs\Console;
 
 use Illuminate\Foundation\Console\ListenerMakeCommand as BaseListenerMakeCommand;
@@ -15,32 +16,28 @@ use Illuminate\Support\Str;
 class ListenerMakeCommand extends BaseListenerMakeCommand
 {
     /**
-     * Get the stub file for the generator.
+     * The Laravel application instance.
      *
-     * @return string
+     * @var \Illuminate\Foundation\Application
      */
-    protected function getStub()
-    {
-        if ($this->option('queued')) {
-            $stub = config('stubs.path').'/listener-queued.stub';
-        } else {
-            $stub = config('stubs.path').'/listener.stub';
-        }
-
-        return file_exists($stub) ? $stub : parent::getStub();
-    }
+    protected $laravel;
 
     /**
      * Get the default namespace for the class.
      *
-     * @param  string  $rootNamespace
+     * @param  string $rootNamespace
      * @return string
      */
     protected function getDefaultNamespace($rootNamespace)
     {
-        return $rootNamespace.config('stubs.namespaces.listener');
+        return $rootNamespace . config('stubs.namespaces.listener');
     }
 
+    /**
+     * @param string $name
+     * @return mixed|string
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
     protected function buildClass($name)
     {
         $stub = $this->files->get($this->getStub());
@@ -52,13 +49,37 @@ class ListenerMakeCommand extends BaseListenerMakeCommand
         return $stub;
     }
 
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        if ($this->option('queued') && $this->option('event')) {
+            $stub = config('stubs.path') . '/listener-queued.stub';
+        } elseif ($this->option('queued')) {
+            $stub = config('stubs.path') . '/listener-queued-duck.stub';
+        } elseif ($this->option('event')) {
+            $stub = config('stubs.path') . '/listener.stub';
+        } else {
+            $stub = config('stubs.path') . '/listener-duck.stub';
+        }
+
+        return file_exists($stub) ? $stub : parent::getStub();
+    }
+
     protected function getEvent()
     {
         $event = $this->option('event');
 
-        if (! Str::startsWith($event, $this->laravel->getNamespace()) && ! Str::startsWith($event, 'Illuminate')) {
-            $eventNamespace = ltrim(config('stubs.namespaces.event'), '\\').'\\';
-            $event = $this->laravel->getNamespace().$eventNamespace.$event;
+        if (!Str::startsWith($event, [
+            $this->laravel->getNamespace(),
+            'Illuminate',
+            '\\',
+        ])) {
+            $eventNamespace = ltrim(config('stubs.namespaces.event'), '\\') . '\\';
+            $event = $this->laravel->getNamespace() . $eventNamespace . $event;
         }
 
         return $event;
