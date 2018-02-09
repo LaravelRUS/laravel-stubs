e1ee336ae6d5bf37d1e4faefd805f65b4aa5506c
atehnix <atehnix@gmail.com>
2018-02-09 15:13:07 +0300

    Add make:channel command

---
+++ src/Console/ChannelMakeCommand.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of laravel-stubs package.
+ *
+ * @author ATehnix <atehnix@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ATehnix\LaravelStubs\Console;
+
+use Illuminate\Foundation\Console\ChannelMakeCommand as BaseChannelMakeCommand;
+
+class ChannelMakeCommand extends BaseChannelMakeCommand
+{
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        $stub = config('stubs.path') . '/channel.stub';
+
+        return file_exists($stub) ? $stub : parent::getStub();
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . config('stubs.namespaces.channel');
+    }
+}
