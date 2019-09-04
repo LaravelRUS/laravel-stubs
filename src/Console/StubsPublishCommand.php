9546272ae22d4bf29027fe216e3fba8bb414c201
atehnix <atehnix@gmail.com>
2019-09-04 06:34:53 +0300

    Improve stubs:publish

--- src/Console/StubsPublishCommand.php
+++ src/Console/StubsPublishCommand.php
@@ -44,64 +44,56 @@ class StubsPublishCommand extends Command
     protected $frameworkPath = 'vendor/laravel/framework/src/';
 
     /**
-     * Paths to stub files
+     * Paths to stub files and a map of their names
      *
      * @var array
      */
     protected $stubs = [
-        'Illuminate/Database/Console/Factories/stubs/factory.stub',
-        'Illuminate/Database/Console/Seeds/stubs/seeder.stub',
-        'Illuminate/Foundation/Console/stubs/channel.stub',
-        'Illuminate/Foundation/Console/stubs/console.stub',
-        'Illuminate/Foundation/Console/stubs/event.stub',
-        'Illuminate/Foundation/Console/stubs/exception-render-report.stub',
-        'Illuminate/Foundation/Console/stubs/exception-render.stub',
-        'Illuminate/Foundation/Console/stubs/exception-report.stub',
-        'Illuminate/Foundation/Console/stubs/exception.stub',
-        'Illuminate/Foundation/Console/stubs/job.stub',
-        'Illuminate/Foundation/Console/stubs/job-queued.stub',
-        'Illuminate/Foundation/Console/stubs/listener-duck.stub',
-        'Illuminate/Foundation/Console/stubs/listener-queued-duck.stub',
-        'Illuminate/Foundation/Console/stubs/listener-queued.stub',
-        'Illuminate/Foundation/Console/stubs/listener.stub',
-        'Illuminate/Foundation/Console/stubs/mail.stub',
-        'Illuminate/Foundation/Console/stubs/markdown-mail.stub',
-        'Illuminate/Foundation/Console/stubs/markdown-notification.stub',
-        'Illuminate/Foundation/Console/stubs/markdown.stub',
-        'Illuminate/Foundation/Console/stubs/model.stub',
-        'Illuminate/Foundation/Console/stubs/notification.stub',
-        'Illuminate/Foundation/Console/stubs/observer.stub',
-        'Illuminate/Foundation/Console/stubs/observer.plain.stub',
-        'Illuminate/Foundation/Console/stubs/pivot.model.stub',
-        'Illuminate/Foundation/Console/stubs/policy.plain.stub',
-        'Illuminate/Foundation/Console/stubs/policy.stub',
-        'Illuminate/Foundation/Console/stubs/provider.stub',
-        'Illuminate/Foundation/Console/stubs/request.stub',
-        'Illuminate/Foundation/Console/stubs/resource-collection.stub',
-        'Illuminate/Foundation/Console/stubs/resource.stub',
-        'Illuminate/Foundation/Console/stubs/rule.stub',
-        'Illuminate/Foundation/Console/stubs/test.stub',
-        'Illuminate/Foundation/Console/stubs/unit-test.stub',
-        'Illuminate/Routing/Console/stubs/controller.api.stub',
-        'Illuminate/Routing/Console/stubs/controller.invokable.stub',
-        'Illuminate/Routing/Console/stubs/controller.model.api.stub',
-        'Illuminate/Routing/Console/stubs/controller.model.stub',
-        'Illuminate/Routing/Console/stubs/controller.nested.api.stub',
-        'Illuminate/Routing/Console/stubs/controller.nested.stub',
-        'Illuminate/Routing/Console/stubs/controller.plain.stub',
-        'Illuminate/Routing/Console/stubs/controller.stub',
-        'Illuminate/Routing/Console/stubs/middleware.stub',
-    ];
-
-    /**
-     * Paths to migration stub files and a map of their names
-     *
-     * @var array
-     */
-    protected $migrationStubs = [
-        'Illuminate/Database/Migrations/stubs/blank.stub' => 'migration.blank.stub',
-        'Illuminate/Database/Migrations/stubs/create.stub' => 'migration.create.stub',
-        'Illuminate/Database/Migrations/stubs/update.stub' => 'migration.update.stub',
+        'channel.stub'                 => 'Illuminate/Foundation/Console/stubs/channel.stub',
+        'console.stub'                 => 'Illuminate/Foundation/Console/stubs/console.stub',
+        'controller.api.stub'          => 'Illuminate/Routing/Console/stubs/controller.api.stub',
+        'controller.invokable.stub'    => 'Illuminate/Routing/Console/stubs/controller.invokable.stub',
+        'controller.model.api.stub'    => 'Illuminate/Routing/Console/stubs/controller.model.api.stub',
+        'controller.model.stub'        => 'Illuminate/Routing/Console/stubs/controller.model.stub',
+        'controller.nested.api.stub'   => 'Illuminate/Routing/Console/stubs/controller.nested.api.stub',
+        'controller.nested.stub'       => 'Illuminate/Routing/Console/stubs/controller.nested.stub',
+        'controller.plain.stub'        => 'Illuminate/Routing/Console/stubs/controller.plain.stub',
+        'controller.stub'              => 'Illuminate/Routing/Console/stubs/controller.stub',
+        'event.stub'                   => 'Illuminate/Foundation/Console/stubs/event.stub',
+        'exception-render-report.stub' => 'Illuminate/Foundation/Console/stubs/exception-render-report.stub',
+        'exception-render.stub'        => 'Illuminate/Foundation/Console/stubs/exception-render.stub',
+        'exception-report.stub'        => 'Illuminate/Foundation/Console/stubs/exception-report.stub',
+        'exception.stub'               => 'Illuminate/Foundation/Console/stubs/exception.stub',
+        'factory.stub'                 => 'Illuminate/Database/Console/Factories/stubs/factory.stub',
+        'job.stub'                     => 'Illuminate/Foundation/Console/stubs/job.stub',
+        'job-queued.stub'              => 'Illuminate/Foundation/Console/stubs/job-queued.stub',
+        'listener-duck.stub'           => 'Illuminate/Foundation/Console/stubs/listener-duck.stub',
+        'listener-queued-duck.stub'    => 'Illuminate/Foundation/Console/stubs/listener-queued-duck.stub',
+        'listener-queued.stub'         => 'Illuminate/Foundation/Console/stubs/listener-queued.stub',
+        'listener.stub'                => 'Illuminate/Foundation/Console/stubs/listener.stub',
+        'mail.stub'                    => 'Illuminate/Foundation/Console/stubs/mail.stub',
+        'markdown-mail.stub'           => 'Illuminate/Foundation/Console/stubs/markdown-mail.stub',
+        'markdown-notification.stub'   => 'Illuminate/Foundation/Console/stubs/markdown-notification.stub',
+        'markdown.stub'                => 'Illuminate/Foundation/Console/stubs/markdown.stub',
+        'middleware.stub'              => 'Illuminate/Routing/Console/stubs/middleware.stub',
+        'migration.blank.stub'         => 'Illuminate/Database/Migrations/stubs/blank.stub',
+        'migration.create.stub'        => 'Illuminate/Database/Migrations/stubs/create.stub',
+        'migration.update.stub'        => 'Illuminate/Database/Migrations/stubs/update.stub',
+        'model.stub'                   => 'Illuminate/Foundation/Console/stubs/model.stub',
+        'notification.stub'            => 'Illuminate/Foundation/Console/stubs/notification.stub',
+        'observer.stub'                => 'Illuminate/Foundation/Console/stubs/observer.stub',
+        'observer.plain.stub'          => 'Illuminate/Foundation/Console/stubs/observer.plain.stub',
+        'pivot.model.stub'             => 'Illuminate/Foundation/Console/stubs/pivot.model.stub',
+        'policy.plain.stub'            => 'Illuminate/Foundation/Console/stubs/policy.plain.stub',
+        'policy.stub'                  => 'Illuminate/Foundation/Console/stubs/policy.stub',
+        'provider.stub'                => 'Illuminate/Foundation/Console/stubs/provider.stub',
+        'request.stub'                 => 'Illuminate/Foundation/Console/stubs/request.stub',
+        'resource-collection.stub'     => 'Illuminate/Foundation/Console/stubs/resource-collection.stub',
+        'resource.stub'                => 'Illuminate/Foundation/Console/stubs/resource.stub',
+        'rule.stub'                    => 'Illuminate/Foundation/Console/stubs/rule.stub',
+        'seeder.stub'                  => 'Illuminate/Database/Console/Seeds/stubs/seeder.stub',
+        'test.stub'                    => 'Illuminate/Foundation/Console/stubs/test.stub',
+        'unit-test.stub'               => 'Illuminate/Foundation/Console/stubs/unit-test.stub',
     ];
 
     /**
@@ -127,13 +119,7 @@ class StubsPublishCommand extends Command
         $this->createDirectory($path);
         $publishedCount = 0;
 
-        foreach ($this->stubs as $stub) {
-            $from = base_path($this->frameworkPath . $stub);
-            $to = $path . '/' . basename($stub);
-            $publishedCount += (int)$this->publishFile($from, $to);
-        }
-
-        foreach ($this->migrationStubs as $stub => $name) {
+        foreach ($this->stubs as $name => $stub) {
             $from = base_path($this->frameworkPath . $stub);
             $to = $path . '/' . $name;
             $publishedCount += (int)$this->publishFile($from, $to);
